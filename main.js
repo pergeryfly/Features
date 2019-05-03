583a8024877457a70c72dc4fcc32737ae9d9b276
pergeryfly <pergeryfly@gmail.com>
2019-05-04 00:54:36 +0800

    ddd

---
+++ main.js
@@ -0,0 +1,3 @@
+$("#p1").click(function(){$('.imgs').css('margin-left','0px')})
+$("#p2").click(function(){$('.imgs').css('margin-left','-200px')})
+$("#p3").click(function(){$('.imgs').css('margin-left','-400px')})
