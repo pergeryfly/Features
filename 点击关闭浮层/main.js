1de842d47f5764d859a57bcfef2c98f9c345a320
pergeryfly <pergeryfly@gmail.com>
2019-05-10 00:12:37 +0800

    添加了一个swiper轮播

--- 点击关闭浮层/main.js
+++ 点击关闭浮层/main.js
@@ -1,15 +1,14 @@
-$('button').on('click', function() {
-    $('.floating').show()
-    console.log('show')
-    setTimeout(function() {
-      console.log('添加 one click')
-      $(document).one('click', function() {
-        console.log('我觉得这里不会执行')
-        console.log('hide')
-        $('.floating').hide()
-      })
-    }, 0)
-  })
+$('button').on('click', function(e) {
+  e.stopPropagation()
+  if($('.floating').is(':hidden')){
+    			$('.floating').show()
+    			}else{
+    				$('.floating').hide()
+    				}
+                })
+$(document).on('click',function(){
+  $('.floating').hide()
+})
   $('.floating').on('click', function(e) {
    e.stopPropagation()
   })
